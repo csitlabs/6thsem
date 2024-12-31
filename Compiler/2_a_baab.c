a3efb8aa0ee3b3a57284afbd91ea62259e029354
Bimochan Poudel <bimochanpoudel2060@gmail.com>
2024-12-31 20:54:35 +0545

    Compiler Lab Done

---
+++ Compiler/2_a_baab.c
@@ -0,0 +1,53 @@
+// Q.  Write a program to DFA that accept string. a. Baab
+
+#include <stdio.h>
+
+int main()
+{
+    char str[20];
+    int i = 0, state = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[i] != '\0')
+    {
+        switch (state)
+        {
+        case 0:
+            if (str[i] == 'b')
+                state = 1;
+            else
+                state = 4;
+            break;
+        case 1:
+            if (str[i] == 'a')
+                state = 2;
+            else
+                state = 4;
+            break;
+        case 2:
+            if (str[i] == 'a')
+                state = 3;
+            else
+                state = 4;
+            break;
+        case 3:
+            if (str[i] == 'b')
+                state = 3;
+            else
+                state = 4;
+            break;
+        case 4:
+            break; // Invalid state
+        }
+        i++;
+    }
+
+    if (state == 3)
+        printf("The string is accepted\n");
+    else
+        printf("The string is invalid\n");
+
+    return 0;
+}
---
+++ Compiler/2_b_abba.c
@@ -0,0 +1,56 @@
+// Q.  Write a program to DFA that accept string. b. abba 
+
+#include <stdio.h>
+
+int main()
+{
+    char str[20];
+    int i = 0, state = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[i] != '\0')
+    {
+        switch (state)
+        {
+        case 0:
+            if (str[i] == 'a')
+                state = 1;
+            else
+                state = 5;
+            break;
+        case 1:
+            if (str[i] == 'b')
+                state = 2;
+            else
+                state = 5;
+            break;
+        case 2:
+            if (str[i] == 'b')
+                state = 3;
+            else
+                state = 5;
+            break;
+        case 3:
+            if (str[i] == 'a')
+                state = 4;
+            else
+                state = 5;
+            break;
+        case 4:
+            state = 5; // Any additional characters after 'abba' will lead to invalid state
+            break;
+        case 5:
+            break; // Invalid state
+        }
+        i++;
+    }
+
+    if (state == 4)
+        printf("The string is accepted\n");
+    else
+        printf("The string is invalid\n");
+
+    return 0;
+}
